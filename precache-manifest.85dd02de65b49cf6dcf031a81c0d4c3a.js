75561d6365e1dc92cb35d381b414a63bf5e10c4c
ArtyomZayarny <artyom.zayarny@gmail.com>
2020-05-28 23:19:58 +0300

    Updates

---
+++ precache-manifest.85dd02de65b49cf6dcf031a81c0d4c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb29c6e80bb75f5014e7f57a078b140",
+    "url": "/Tomnium/index.html"
+  },
+  {
+    "revision": "4d3df02176d0ba86c489",
+    "url": "/Tomnium/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c2556b12a555c0a3710",
+    "url": "/Tomnium/static/js/2.f3fca92f.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Tomnium/static/js/2.f3fca92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3df02176d0ba86c489",
+    "url": "/Tomnium/static/js/main.7b8f214f.chunk.js"
+  },
+  {
+    "revision": "5b5cf2ac3990f0e25444",
+    "url": "/Tomnium/static/js/runtime-main.d5210559.js"
+  }
+]);
